c8529d3c3174d1c7eb2f24a1e8685fcc346c8581
Andy Perrotte <andy.perrotte@hotmail.com>
2026-05-12 00:15:14 +0200

    done

--- src/index.js
+++ src/index.js
@@ -2,32 +2,51 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
-
+// On récupère les éléments HTML
+const startButton = document.querySelector("#start-btn");
+const timeElement = document.querySelector("#time");
+const toastElement = document.querySelector("#toast");
+const toastMessage = document.querySelector("#toast-message");
 
 // ITERATION 1: Add event listener to the start button
-
-// Your code goes here ...
-
-
-
+startButton.addEventListener("click", startCountdown);
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
 
+  remainingTime = DURATION;
+  timeElement.textContent = remainingTime;
 
-  // Your code goes here ...
+  startButton.disabled = true;
+
+  timer = setInterval(function () {
+    remainingTime--;
+
+    timeElement.textContent = remainingTime;
+
+    if (remainingTime === 0) {
+      clearInterval(timer);
+
+      startButton.disabled = false;
+
+      showToast("Lift off! 🚀");
+    }
+  }, 1000);
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
 
-  // Your code goes here ...
+  toastMessage.textContent = message;
 
+  toastElement.classList.add("show");
+
+  setTimeout(function () {
+    toastElement.classList.remove("show");
+  }, 3000);
+}
 
 
 
@@ -35,4 +54,4 @@ function showToast(message) {
 
   // Your code goes here ...
 
-}
+
